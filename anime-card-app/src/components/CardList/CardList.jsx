7ce170ae907aa4d3adc2d2daf9bdefca6d4f0b7f
ryaba-ya <ksushkenchik@bk.ru>
2023-09-07 13:21:20 +0200

    mist

--- anime-card-app/src/components/CardList/CardList.jsx
+++ anime-card-app/src/components/CardList/CardList.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import Card from '../Card';
+import Card from '../Card/Card';
 import axios from 'axios';
 
 const CardList = ({ onLikeClick, onDeleteClick }) => {
