8012fe2f8d3edfced27d2a76991f657045b92f9c
ryaba-ya <ksushkenchik@bk.ru>
2023-09-08 23:17:03 +0200

    j

--- anime-card-app/src/components/Card/Card.jsx
+++ anime-card-app/src/components/Card/Card.jsx
@@ -3,24 +3,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CardContainer = styled.div`
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
-const CardImage = styled.img`
-  max-width: 100%;
-  height: auto;
-`;
-
-const CardInfo = styled.div`
-  margin-top: 10px;
-  text-align: center;
-`;
 
 
 function Card(props) {
