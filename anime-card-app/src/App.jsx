e407538b8369d7d01f8551e919065c9cdf6a187a
ryaba-ya <ksushkenchik@bk.ru>
2023-09-08 23:14:05 +0200

    s

--- anime-card-app/src/App.jsx
+++ anime-card-app/src/App.jsx
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import Card from '../src/components/Card/Card'
 function App() {
-  
-   <Card>
-    
-   </Card>
+  return (
+    <div className="App">
+      {
+        cardData.map((elem, index) =>
+          <Card key={index} title={elem.title}
+            price={elem.price}
+            description={elem.description}
+            imgLink={elem.imgLink}
+          ></Card>
+        )
+      }
+    </div>
+  );
 }
 
 export default App;
--- anime-card-app/src/components/CardList/CardList.jsx
+++ anime-card-app/src/components/CardList/CardList.jsx
@@ -3,29 +3,7 @@ import React from 'react'
 import Card from '../Card/Card';
 
 
-function CardList() {
-	return (
-		<React.Fragment>
-			<Card
-				title="Зимние ботинки"
-				price={99}
-				description="Такие только у нас"
-				imgLink="https://shop/winter_boots.jpg"
-			/>
-			<Card
-				title="Пляжные тапки"
-				price={23}
-				description="Надеюсь, пригодятся"
-				imgLink="https://shop/slippers.jpg"
-			/>
-			<Card
-				title="Праздничные туфли"
-				price={85}
-				description="Теперь и в чёрном цвете"
-				imgLink="https://shop/shoes.jpg"
-			/>
-		</React.Fragment>
-	);
-}
+
+
 
 export default CardList;
