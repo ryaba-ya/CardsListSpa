d60c3beced5b18faa0583f9b7d968a12edbe28a9
ryaba-ya <ksushkenchik@bk.ru>
2023-09-08 13:17:56 +0200

    cha

--- anime-card-app/src/components/CardList/CardList.jsx
+++ anime-card-app/src/components/CardList/CardList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card from './Card';
+import Card from '../Card/Card';
 
 const CardListContainer = styled.div`
   display: flex;
